4594f1eeb21c63cb9573bb9a08ddded2b9ff205a
Lamin <lamin@live.com>
2020-01-22 22:44:09 +0700

    Update process.py

--- process.py
+++ process.py
@@ -1,23 +1,17 @@
 import glob, os
 
-# Current directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 print(current_dir)
 name = input("Enter")
 current_dir = 'Your dataset path.'
 
-# Directory where the data will reside, relative to 'darknet.exe'
-#path_data = './NFPAdataset/'
-
-# Percentage of images to be used for the test set
 percentage_test = 10;
 
-# Create and/or truncate train.txt and test.txt
+
 file_train = open('train.txt', 'w')
 file_test = open('test.txt', 'w')
 
-# Populate train.txt and test.txt
 counter = 1
 index_test = round(100 / percentage_test)
 for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):
